4aa86a5eb25b3e6276e00206596f7cefbf722eb3
Tom MOUSSET <62646888+pytomprog@users.noreply.github.com>
2023-11-16 16:25:37 +0100

    instance creation added

--- vulkanRenderer/main.cpp
+++ vulkanRenderer/main.cpp
@@ -21,6 +21,8 @@ public:
 private:
     GLFWwindow* window;
 
+    VkInstance instance;
+
     void initWindow() {
         glfwInit();
 
@@ -31,7 +33,7 @@ private:
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -41,9 +43,51 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::cout << "Extensions requises :" << std::endl;
+        for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+            std::cout << '\t' << glfwExtensions[i] << std::endl;
+        }
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("Echec de la création de l'instance!");
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        std::cout << "Extensions disponibles :" << std::endl;
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << std::endl;
+        }
     }
 };
 
